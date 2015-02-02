de148fa54e8bb19e971f504553c7bb58ac88a56c
dgrtwo <dgrtwo@princeton.edu>
2015-02-02 13:43:22 -0500

    Adjustments to title and text; link to blog post

--- ui.R
+++ ui.R
@@ -10,12 +10,12 @@ msg <- paste("<span style = 'font-size: 11px'>",
 
 shinyUI(fluidPage(
   # Application title
-  titlePanel("Personalized Stack Exchange Prediction Viewer"),
+  titlePanel("How StackOverflow Sees You"),
 
   # Sidebar with a slider input for number of bins
   sidebarLayout(
     sidebarPanel(
-      fileInput("data_file", "Upload personalized prediction data",
+      fileInput("data_file", "Upload Stack Exchange personalized prediction data:",
                 accept = "text/json"),
       HTML(msg),
       HTML("<div style = 'height: 25px'></div>"),
@@ -29,21 +29,22 @@ shinyUI(fluidPage(
                   min = 1,
                   max = 16,
                   value = 6),
-       HTML("<span style = 'font-size: 11px'>",
-            "<p>This app is <i>not</i> affiliated with Stack Exchange. It also does",
-            "not permanently store any of your data or other information about you.",
-            "The Shiny Apps Terms of Use, including terms of use for uploaded files,",
-            "can be found",
-            "<a href='http://www.rstudio.com/about/shiny-apps-terms-use/'>here</a>.",
-            "</p>"),
-      HTML("<p>See <a href='https://github.com/dgrtwo/providence-viewer'>",
-           "here</a> for the Shiny code used for this app.</p>"),
-      HTML("<p>See <a href='http://kevinmontrose.com/2015/01/29/providence-what-technologies-do-you-know/'>",
+      HTML("<span style = 'font-size: 11px'><ul>"),
+      HTML("<li>See <a href='http://varianceexplained.org/r/providence-visualizer/'>here</a>",
+           "for more about how and why I made this app.</li>"),
+      HTML("<li>See <a href='https://github.com/dgrtwo/providence-viewer'>",
+           "here</a> for the code.</li>"),
+      HTML("<li>See <a href='http://kevinmontrose.com/2015/01/29/providence-what-technologies-do-you-know/'>",
            "here</a> for more about how Stack Exchange's Providence system",
            "constructs these predictions",
-           "based on your traffic on the site.</p></span>")
+           "based on your traffic on the site.</li></span>"),
+      HTML("<li>This app is <i>not</i> affiliated with Stack Exchange. It also does",
+           "not permanently store any of your data or other information about you.",
+           "The Shiny Apps Terms of Use, including terms of use for uploaded files,",
+           "can be found",
+           "<a href='http://www.rstudio.com/about/shiny-apps-terms-use/'>here</a>.",
+           "</li>")
     ),
-
     # Show a plot of the generated distribution
     mainPanel(
       plotOutput("categories_plot")
